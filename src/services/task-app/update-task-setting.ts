72885475111f3cddfb8c47034faad060522a54c0
Irzan Maulana <maulanairzan5@gmail.com>
2025-06-11 00:17:39 +0700

    fix : fix not delete form settings

--- src/services/task-app/update-task-setting.ts
+++ src/services/task-app/update-task-setting.ts
@@ -1,18 +1,36 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { formSettingContainer } from "../../databases/task-app/task-app";
 import { SuccessResponse, ErrorResponse } from "../../utils/response-handler";
+import { RequiredBodyParams } from "../../utils/required-params";
 
 export async function UpdateTaskSetting(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
     try {
-        const formSettings = await request.json() as any[];
-        for (const formSetting of formSettings) {
+        const { organizationId, data } = await RequiredBodyParams(request, ["organizationId"]) as any;
+
+        const query = `SELECT c.id FROM c WHERE c.organizationId = @organizationId`;
+        const { resources: existingFormSetting } = await formSettingContainer.items
+            .query({
+                query,
+                parameters: [{ name: "@organizationId", value: organizationId }]
+            })
+            .fetchAll();
+
+        const existingIds = new Set(existingFormSetting.map((t: any) => t.id));
+        const newIds = new Set(data.map(t => t.id));
+
+        const idsToDelete = [...existingIds].filter(id => !newIds.has(id));
+        await Promise.all(idsToDelete.map(id =>
+            formSettingContainer.item(id, organizationId).delete()
+        ));
+
+        for (const formSetting of data) {
             if (formSetting?.organizationId === undefined) {
                 continue;
             }
             await formSettingContainer.items.upsert(formSetting);
         }
-        return SuccessResponse(formSettings, "Task Setting updated successfully");
+        return SuccessResponse(data, "Task Setting updated successfully");
     } catch (error: any) {
         return ErrorResponse(context, error, "Error in UpdateTaskSetting");
     }
